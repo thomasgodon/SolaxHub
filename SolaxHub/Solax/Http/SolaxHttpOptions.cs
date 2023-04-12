4654556a470c306f22c643a056edc315806c2c97
Thomas Godon <tgodon@inimco.com>
2023-04-12 21:52:13 +0200

    solax http client changes

--- SolaxHub/Solax/Http/SolaxHttpOptions.cs
+++ SolaxHub/Solax/Http/SolaxHttpOptions.cs
@@ -5,5 +5,6 @@
         public string TokenId { get; init; } = null!;
         public string SerialNumber { get; init; } = null!;
         public TimeSpan PollInterval { get; init; } = TimeSpan.FromSeconds(7);
+        public bool Enabled { get; init; } = false;
     }
 }
