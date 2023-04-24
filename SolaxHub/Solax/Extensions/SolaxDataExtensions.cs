12e9ed4ed3fff8c61379d7415f57658daa3d2ce8
Thomas Godon <thomas.godon@gmail.com>
2023-04-24 22:15:47 +0200

    feat: make udp port mapping configurable (#3)
    * make udp options adaptable
    
    * adapt appsettings

--- SolaxHub/Solax/Extensions/SolaxDataExtensions.cs
+++ SolaxHub/Solax/Extensions/SolaxDataExtensions.cs
@@ -7,15 +7,6 @@ namespace SolaxHub.Solax.Extensions
     internal static class SolaxDataExtensions
     {
 
-        public static byte[] ToUdpPacket(this SolaxData result, string property)
-        {
-            var value = result.GetType().GetProperty(property)?.GetValue(result, null);
-            var subValue = value?.GetType().GetProperty("Value")?.GetValue(value, null);
-            var subSubValue = subValue?.GetType().GetProperty("Value")?.GetValue(subValue, null);
-
-            return Encoding.UTF8.GetBytes(subSubValue?.ToInvariantString() ?? subValue?.ToInvariantString() ?? value?.ToInvariantString() ?? string.Empty);
-        }
-
         private static string? ToInvariantString(this object value)
         {
             if (value is decimal decimalValue)
