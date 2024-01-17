08d0b214249f9bd2d906c76adfd66a0c338211fc
Thomas Godon <thomas.godon@gmail.com>
2024-01-17 20:20:02 +0100

    fix write

--- SolaxHub/Solax/Modbus/Client/SolaxModbusWriters.cs
+++ SolaxHub/Solax/Modbus/Client/SolaxModbusWriters.cs
@@ -19,21 +19,7 @@ internal partial class SolaxModbusClient
     public async Task SetModbusPowerControlAsync(bool enabled, double activePower, double reactivePower, CancellationToken cancellationToken)
     {
         const ushort registerAddress = 0x007C;
-        var dataSet = GetPowerControlDataSet(enabled, (short)activePower, (short)reactivePower);
+        var dataSet = new[] { Convert.ToSingle(enabled), Convert.ToSingle(activePower), Convert.ToSingle(reactivePower) };
         await _modbusClient.WriteMultipleRegistersAsync(UnitIdentifier, registerAddress, dataSet, cancellationToken);
-    }
-
-    private static byte[] GetPowerControlDataSet(bool enabled, short activePower, short reactivePower)
-    {
-        var enabledValue = BitConverter.GetBytes(enabled);
-        var activePowerValue = BitConverter.GetBytes(activePower);
-        var reactivePowerValue = BitConverter.GetBytes(reactivePower);
-
-        var dataSet = new byte[enabledValue.Length + activePowerValue.Length + reactivePowerValue.Length];
-        Buffer.BlockCopy(enabledValue, 0, dataSet, 0, enabledValue.Length);
-        Buffer.BlockCopy(activePowerValue, 0, dataSet, enabledValue.Length, activePowerValue.Length);
-        Buffer.BlockCopy(reactivePowerValue, 0, dataSet, enabledValue.Length + activePowerValue.Length, reactivePowerValue.Length);
-
-        return dataSet;
     }
 }
