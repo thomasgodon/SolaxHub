c3b6ba8d7698c37d1a6c07195ece2068579681af
Thomas Godon <thomas.godon@gmail.com>
2025-02-28 22:36:37 +0100

    fix

--- SolaxHub/Solax/Queries/Handlers/GetConsumeEnergyQueryHandler.cs
+++ SolaxHub/Solax/Queries/Handlers/GetConsumeEnergyQueryHandler.cs
@@ -1,5 +1,6 @@
 using MediatR;
 using SolaxHub.Solax.Modbus.Client;
+using SolaxHub.Solax.Registers;
 
 namespace SolaxHub.Solax.Queries.Handlers;
 
@@ -14,9 +15,8 @@ public class GetConsumeEnergyQueryHandler : IRequestHandler<GetConsumeEnergyQuer
 
     public async Task<double> Handle(GetConsumeEnergyQuery request, CancellationToken cancellationToken)
     {
-        const ushort startingAddress = 74;
-        const ushort count = 2;
-        Memory<byte> data = await _solaxModbusClient.ReadInputRegistersAsync(startingAddress, count, cancellationToken);
+        const ushort quantity = 2;
+        Memory<byte> data = await _solaxModbusClient.ReadInputRegistersAsync(ReadInputRegisters.ConsumeEnergyTotal, quantity, cancellationToken);
         return Math.Round((data.ToArray()[1] << 16 | data.ToArray()[0] & 0xffff) * 0.01, 2);
     }
 }
--- SolaxHub/Solax/Registers/ReadInputRegisters.cs
+++ SolaxHub/Solax/Registers/ReadInputRegisters.cs
@@ -5,4 +5,5 @@ public static class ReadInputRegisters
     public const ushort BatPowerCharge1 = 0x0016;
     public const ushort BatteryCapacity = 0x001C;
     public const ushort LockState = 0x0054;
+    public const ushort ConsumeEnergyTotal = 0x004A;
 }
