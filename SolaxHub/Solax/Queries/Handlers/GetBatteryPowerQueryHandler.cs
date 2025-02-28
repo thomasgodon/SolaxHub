461b5c94396e87ac6be6728c850038e248353e64
Thomas Godon <thomas.godon@gmail.com>
2025-02-28 22:32:37 +0100

    fix batt power

--- SolaxHub/Solax/Queries/Handlers/GetBatteryPowerQueryHandler.cs
+++ SolaxHub/Solax/Queries/Handlers/GetBatteryPowerQueryHandler.cs
@@ -1,5 +1,6 @@
 using MediatR;
 using SolaxHub.Solax.Modbus.Client;
+using SolaxHub.Solax.Registers;
 
 namespace SolaxHub.Solax.Queries.Handlers;
 
@@ -14,9 +15,8 @@ public class GetBatteryPowerQueryHandler : IRequestHandler<GetBatteryPowerQuery,
 
     public async Task<short> Handle(GetBatteryPowerQuery request, CancellationToken cancellationToken)
     {
-        const ushort startingAddress = 22;
-        const ushort count = 1;
-        Memory<byte> data = await _solaxModbusClient.ReadInputRegistersAsync(startingAddress, count, cancellationToken);
+        const ushort quantity = 1;
+        Memory<byte> data = await _solaxModbusClient.ReadInputRegistersAsync(ReadInputRegisters.BatPowerCharge1, quantity, cancellationToken);
         return data.ToArray()[0];
     }
 }
--- SolaxHub/Solax/Registers/ReadInputRegisters.cs
+++ SolaxHub/Solax/Registers/ReadInputRegisters.cs
@@ -2,6 +2,7 @@
 
 public static class ReadInputRegisters
 {
+    public const ushort BatPowerCharge1 = 0x0016;
     public const ushort BatteryCapacity = 0x001C;
     public const ushort LockState = 0x0054;
 }
