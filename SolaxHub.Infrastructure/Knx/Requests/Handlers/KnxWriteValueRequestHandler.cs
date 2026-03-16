c91ff464a5752027fe15a896b6b7529d26ab5d7b
Thomas Godon <thomas.godon@gmail.com>
2026-03-16 13:49:58 +0100

    Add BatteryMaxDischargePower support and config updates
    Added handling for BatteryMaxDischargePower in KnxWriteValueRequestHandler to allow setting or disabling the battery discharge power limit. Updated appsettings.json to include the new group address and writable parameter. Added a Bash command in settings.local.json for searching BatteryMaxDischarge and BatteryDischarge references.

--- SolaxHub.Infrastructure/Knx/Requests/Handlers/KnxWriteValueRequestHandler.cs
+++ SolaxHub.Infrastructure/Knx/Requests/Handlers/KnxWriteValueRequestHandler.cs
@@ -77,6 +77,23 @@ internal class KnxWriteValueRequestHandler : IRequestHandler<KnxWriteValueReques
                 _powerControlBuffer.SetChargeDischargePower(chargeDischargePower);
                 break;
 
+            case "BatteryMaxDischargePower":
+                var maxDischargePower = (int)BitConverter.ToSingle(request.Value);
+                if (maxDischargePower <= 0)
+                {
+                    _logger.LogInformation("Disabling battery discharge power limit");
+                    var disableData = _powerControlBuffer.BuildRegisterBlock(PowerControlMode.Disabled);
+                    await _sender.Send(new SetPowerControlCommand(PowerControlMode.Disabled, disableData), cancellationToken);
+                }
+                else
+                {
+                    _logger.LogInformation("Setting battery max discharge power to {Watts}W", maxDischargePower);
+                    _powerControlBuffer.SetChargeDischargePower(maxDischargePower);
+                    var selfConsumeData = _powerControlBuffer.BuildRegisterBlock(PowerControlMode.SelfConsumeChargeDischargeMode);
+                    await _sender.Send(new SetPowerControlCommand(PowerControlMode.SelfConsumeChargeDischargeMode, selfConsumeData), cancellationToken);
+                }
+                break;
+
             default:
                 _logger.LogWarning("Writing parameter '{Parameter}' not implemented", capability);
                 break;
